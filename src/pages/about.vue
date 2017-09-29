7e7c430e83bcff5f7a04a596ece38da2f17477c6
alirzayev <alirzayev.07@gmail.com>
2017-09-29 18:47:56 +0400

    ** Update ** About screen design updated.

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,55 +1,61 @@
 <template>
-  <f7-page class="about-page">
-    <f7-navbar :title="$t('app.about')" :back-link="$t('app.back')" sliding></f7-navbar>
-    <f7-block>
-        <div class="logo">
-            <img src="../../static/img/hiapp_logo@2x.png" alt="logo">
-        </div>
-        <div class="app-name">Beedle</div>
-    </f7-block>
-    <f7-block class="detail">
-        <p>Web: caspiansoft.com</p>
-        <p>Email: info@caspiansoft.net</p>
-        <p>Twitter: @caspiansoft</p>
-    </f7-block>
-    <f7-block class="copyright">
-        Copyright © 2017 CaspianSoft.
-    </f7-block>
-  </f7-page>
+    <f7-page class="about-page">
+        <f7-navbar :title="$t('app.about')" :back-link="$t('app.back')" sliding></f7-navbar>
+        <f7-block>
+            <div class="logo">
+                <img src="../../static/img/beedle_logo@2x.png" alt="logo">
+            </div>
+            <div class="app-name">Beedle</div>
+        </f7-block>
+        <f7-block>
+            <div class="row">
+                <div class="col-lg-1 col-centered detail">
+                    <p>
+                        <f7-icon f7="email_fill"></f7-icon>
+                        info@beedle.az
+                    </p>
+                    <p>
+                        <f7-icon f7="social_twitter_fill"></f7-icon>
+                        @beedle
+                    </p>
+                </div>
+            </div>
+        </f7-block>
+
+        <f7-block class="copyright">
+            Copyright © 2017. Developed by Caspian Soft.
+        </f7-block>
+    </f7-page>
 </template>
 
 <style lang="less">
     .about-page {
-        .logo{
+        .logo {
             width: 90px;
             height: 90px;
             line-height: 90px;
-            background-color: #fff; 
+            background-color: #fff;
             text-indent: -9999;
             border-radius: 5px;
             margin: 0 auto;
             border: 1px solid #dfdfdf;
             text-align: center;
-            > img{
+            > img {
                 width: 75px;
                 height: 75px;
                 vertical-align: middle;
             }
         }
-        .app-name{
+        .app-name {
             text-align: center;
-            color: #666;            
+            color: #666;
             font-size: 20px;
             margin-top: 15px;
         }
-        .detail{
-            text-align: center;
+        .detail {
             color: #666;
-            > p{
-                margin: 6px 0;
-            }
         }
-        .copyright{
+        .copyright {
             position: absolute;
             left: 0;
             text-align: center;
@@ -58,6 +64,10 @@
             color: #9c9c9c;
             width: 100%;
             padding: 0;
+        }
+        .col-centered {
+            float: none;
+            margin: 0 auto;
         }
     }
 </style>
